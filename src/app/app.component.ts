2b3fca2dfd8543000b376b464ee223a835f86249
chandu21 <chandu.basina23@gmail.com>
2017-04-19 16:58:17 +0530

    adding the angular project tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "Welcome to Instagram Search";
+  linkfirst = 'https://api.instagram.com/v1/tags/'
+  linksecond = '/media/recent?access_token=2360757869.1c5f91c.5487d58ee14e4168ae9edfe91bec427f';
+  http: Http;
+  giphies = [];
+  constructor(http: Http) { 
+    this.http = http; 
+  }
+  getImages(searchTerm: HTMLInputElement): void { 
+    var apiLink = this.linkfirst + searchTerm.value + this.linksecond;
+  	this.http.request(apiLink).subscribe((res: Response) => { 
+  	   this.giphies = res.json().data;
+  	   console.log(res.json()); 
+  	});
+  }
 }
+
